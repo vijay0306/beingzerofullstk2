dac270cb0f30edf23f9031c373dbbd5ac24dc62f
HarshaReddyVardhan <mhvr226@gmail.com>
2020-01-02 16:01:03 +0530

    asdfs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule}
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PricingComponent,
+    FeaturesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
